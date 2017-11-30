471998ed8ffc2f09343dd620ac0f78e7efdf8f8d
Malgorzata Kudlak <m.kudlak@icloud.com>
2017-11-30 13:18:14 +0100

    Adding changes to scripts for exe3

--- zad3/scripts.js
+++ zad3/scripts.js
@@ -4,4 +4,11 @@ var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
     dinosaur = 'triceratops'.toUpperCase();
     dinosChange = text.replace('Velociraptor', dinosaur);
 
+/* Takie tam dla mnie :P
 console.log(dinosChange);
+
+console.log(dinosChange.length/2);
+*/
+
+console.log(dinosChange.substr(0, dinosChange.length/2));
+
